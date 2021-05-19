d1b74447f2081790793d33eed805fe7c6ec640a3
Andrew Esteban <andrewesteban1999@gmail.com>
2021-05-19 18:00:59 +1000

    read in credentials with Runner

--- src/main/java/food/Runner.java
+++ src/main/java/food/Runner.java
@@ -1,8 +1,47 @@
 package food;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Runner {
 
     public static void main(String[] args) {
+        Map<String, String> credentials = credentialsParser("credentials.json");
 
+        if (credentials == null) {
+            return;
+        }
+
+    }
+
+    private static Map<String, String> credentialsParser(String file) {
+        try {
+            JSONParser parser = new JSONParser();
+            Reader reader = new FileReader(file);
+
+            JSONObject json = (JSONObject) parser.parse(reader);
+
+            Map<String, String> res = new HashMap<>();
+
+            res.put("food-id", (String) json.get("food-id"));
+            res.put("food-key", (String) json.get("food-key"));
+            res.put("twilio-sid", (String) json.get("twilio-sid"));
+            res.put("twilio-token", (String) json.get("twilio-token"));
+
+            return res;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            return null;
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
     }
 }
