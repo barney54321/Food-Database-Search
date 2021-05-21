017b1073e6d40adda9967252fcb77600ce262cfa
Andrew Esteban <andrewesteban1999@gmail.com>
2021-05-21 17:44:51 +1000

    created stub Nutrition classes

---
+++ src/main/java/food/model/IngredientImpl.java
@@ -0,0 +1,40 @@
+package food.model;
+
+import org.json.simple.JSONObject;
+
+public class IngredientImpl implements Ingredient {
+
+    public IngredientImpl(JSONObject json) {
+
+    }
+
+    @Override
+    public String getURI() {
+        return null;
+    }
+
+    @Override
+    public Double getQuantity() {
+        return null;
+    }
+
+    @Override
+    public String getMeasure() {
+        return null;
+    }
+
+    @Override
+    public Double getWeight() {
+        return null;
+    }
+
+    @Override
+    public String getFood() {
+        return null;
+    }
+
+    @Override
+    public String getFoodID() {
+        return null;
+    }
+}
---
+++ src/main/java/food/model/NutrientImpl.java
@@ -0,0 +1,58 @@
+package food.model;
+
+import org.json.simple.JSONObject;
+
+import java.util.List;
+import java.util.Map;
+
+public class NutrientImpl implements Nutrition {
+
+    public NutrientImpl(JSONObject json) {
+
+    }
+
+    @Override
+    public String getURI() {
+        return null;
+    }
+
+    @Override
+    public Integer getCalories() {
+        return null;
+    }
+
+    @Override
+    public Double getTotalWeight() {
+        return null;
+    }
+
+    @Override
+    public List<String> getDietLabels() {
+        return null;
+    }
+
+    @Override
+    public List<String> getHealthLabels() {
+        return null;
+    }
+
+    @Override
+    public List<String> getCautions() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Nutrient> getTotalNutrients() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Nutrient> getTotalDaily() {
+        return null;
+    }
+
+    @Override
+    public List<Ingredient> getIngredients() {
+        return null;
+    }
+}
