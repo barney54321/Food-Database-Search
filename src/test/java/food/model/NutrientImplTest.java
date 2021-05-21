9ca2a2037ba899ed7f48b8ff513c3ce7e9ea2500
Andrew Esteban <andrewesteban1999@gmail.com>
2021-05-21 18:13:08 +1000

    RED: base Nutrient class

---
+++ src/test/java/food/model/NutrientImplTest.java
@@ -0,0 +1,42 @@
+package food.model;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileReader;
+import java.io.Reader;
+
+import static org.junit.Assert.*;
+
+public class NutrientImplTest {
+
+    private Nutrient nutrient;
+
+    @Before
+    public void setUp() throws Exception {
+        Reader reader = new FileReader("src/test/resources/fat.json");
+
+        JSONParser parser = new JSONParser();
+
+        JSONObject json = (JSONObject) parser.parse(reader);
+
+        this.nutrient = new NutrientImpl(json);
+    }
+
+    @Test
+    public void getLabel() {
+        assertEquals("Fat", nutrient.getLabel());
+    }
+
+    @Test
+    public void getQuantity() {
+        assertEquals(0.246, nutrient.getQuantity(), 0.01);
+    }
+
+    @Test
+    public void getUnit() {
+        assertEquals("g", nutrient.getUnit());
+    }
+}
