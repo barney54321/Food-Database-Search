56bd69726a95580b5d2f8ed18cbb77f3e6ff35ce
Andrew Esteban <andrewesteban1999@gmail.com>
2021-05-21 15:14:11 +1000

    GREEN: base Food model

--- src/main/java/food/model/FoodImpl.java
+++ src/main/java/food/model/FoodImpl.java
@@ -1,7 +1,9 @@
 package food.model;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -9,57 +11,96 @@ import java.util.Map;
  */
 public class FoodImpl implements Food {
 
-    public FoodImpl(JSONObject json) {
+    private String id;
+    private String label;
+    private Map<String, Double> nutrients;
+    private String brand;
+    private String category;
+    private String categoryLabel;
+    private String foodContentsLabel;
+    private String imagePath;
+    private Double servingsPerContainer;
+    private Map<String, Double> measures;
 
+    public FoodImpl(JSONObject json) {
+        JSONObject food = (JSONObject) json.get("food");
+
+        this.id = (String) food.get("foodId");
+        this.label = (String) food.get("label");
+        this.brand = (String) food.get("brand");
+        this.category = (String) food.get("category");
+        this.categoryLabel = (String) food.get("categoryLabel");
+        this.foodContentsLabel = (String) food.get("foodContentsLabel");
+        this.imagePath = (String) food.get("image");
+        this.servingsPerContainer = (Double) food.get("servingsPerContainer");
+
+        this.nutrients = new HashMap<String, Double>();
+
+        JSONObject nutrients = (JSONObject) food.get("nutrients");
+
+        this.nutrients.put("ENERC_KCAL", (Double) nutrients.get("ENERC_KCAL"));
+        this.nutrients.put("PROCNT", (Double) nutrients.get("PROCNT"));
+        this.nutrients.put("FAT", (Double) nutrients.get("FAT"));
+        this.nutrients.put("CHOCDF", (Double) nutrients.get("CHOCDF"));
+        this.nutrients.put("FIBTG", (Double) nutrients.get("FIBTG"));
+
+        this.measures = new HashMap<>();
+
+        JSONArray measures = (JSONArray) json.get("measures");
+
+        for (Object o : measures) {
+            JSONObject measure = (JSONObject) o;
+            this.measures.put((String) measure.get("label"), (Double) measure.get("weight"));
+        }
     }
 
     @Override
     public String getID() {
-        return null;
+        return this.id;
     }
 
     @Override
     public String getLabel() {
-        return null;
+        return this.label;
     }
 
     @Override
     public Map<String, Double> getNutrients() {
-        return null;
+        return this.nutrients;
     }
 
     @Override
     public String getBrand() {
-        return null;
+        return this.brand;
     }
 
     @Override
     public String getCategory() {
-        return null;
+        return this.category;
     }
 
     @Override
     public String getCategoryLabel() {
-        return null;
+        return this.categoryLabel;
     }
 
     @Override
     public String getFoodContentsLabel() {
-        return null;
+        return this.foodContentsLabel;
     }
 
     @Override
     public String getImagePath() {
-        return null;
+        return this.imagePath;
     }
 
     @Override
     public Double getServingsPerContainer() {
-        return null;
+        return this.servingsPerContainer;
     }
 
     @Override
     public Map<String, Double> getMeasures() {
-        return null;
+        return this.measures;
     }
 }
