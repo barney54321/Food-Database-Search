8ebf54a5311e505f3ac570d4ed14961dc3d36e4a
Andrew Esteban <andrewesteban1999@gmail.com>
2021-05-21 18:03:03 +1000

    GREEN: base Ingredient class

--- src/main/java/food/model/IngredientImpl.java
+++ src/main/java/food/model/IngredientImpl.java
@@ -4,32 +4,61 @@ import org.json.simple.JSONObject;
 
 public class IngredientImpl implements Ingredient {
 
-    public IngredientImpl(JSONObject json) {
+    /**
+     * Quantity of specified measure.
+     */
+    private Double quantity;
 
+    /**
+     * Measurement unit used for quantity.
+     */
+    private String measure;
+
+    /**
+     * Total weight in grams.
+     */
+    private Double weight;
+
+    /**
+     * The source food item for the ingredient.
+     */
+    private String food;
+
+    /**
+     * The ID of the source food item.
+     */
+    private String foodID;
+
+    public IngredientImpl(JSONObject json) {
+        this.quantity = ((Long) json.get("quantity")).doubleValue();
+        this.measure = (String) json.get("measure");
+        this.weight = ((Long) json.get("weight")).doubleValue();
+        this.food = (String) json.get("food");
+        this.foodID = (String) json.get("foodId");
     }
 
     @Override
     public Double getQuantity() {
-        return null;
+        return this.quantity;
     }
 
     @Override
     public String getMeasure() {
-        return null;
+        return this.measure;
     }
 
     @Override
     public Double getWeight() {
-        return null;
+        return this.weight;
     }
 
     @Override
     public String getFood() {
-        return null;
+        return this.food;
     }
 
     @Override
     public String getFoodID() {
-        return null;
+        return this.foodID;
     }
 }
