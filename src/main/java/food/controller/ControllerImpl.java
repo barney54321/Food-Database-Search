26b77b8f9225dbc92344fea31e86c353a6d88fad
Andrew Esteban <andrewesteban1999@gmail.com>
2021-05-24 22:43:38 +1000

    GREEN: connected Controller to Facade

--- src/main/java/food/controller/ControllerImpl.java
+++ src/main/java/food/controller/ControllerImpl.java
@@ -1,37 +1,57 @@
 package food.controller;
 
 import food.model.ModelFacade;
-import food.model.input.FoodDatabase;
-import food.model.output.Twilio;
 import food.model.models.Food;
-import food.model.models.Nutrition;
 import food.view.observers.FoodListObserver;
 import food.view.observers.MessageObserver;
 import food.view.observers.NutritionObserver;
-
-import java.util.List;
 
 /**
  * Concrete implementation of Controller interface.
  */
 public class ControllerImpl implements Controller {
 
-    public ControllerImpl(ModelFacade facade) {
+    /**
+     * The Facade to interact with.
+     */
+    private ModelFacade facade;
 
+    /**
+     * Creates a new Controller object.
+     * @param facade The Facade to interact with.
+     */
+    public ControllerImpl(ModelFacade facade) {
+        this.facade = facade;
     }
 
     @Override
     public void search(String term, FoodListObserver observer) {
-
+        if (term == null || term.equals("")) {
+            observer.update(new IllegalArgumentException("Search term cannot be empty"));
+        } else {
+            this.facade.search(term, observer);
+        }
     }
 
     @Override
     public void getNutrition(Food food, String measure, NutritionObserver observer) {
-
+        if (food == null) {
+            observer.update(new IllegalArgumentException("Food cannot be null"));
+        } else if (measure == null || measure.equals("")) {
+            observer.update(new IllegalArgumentException("Measure cannot be empty"));
+        } else {
+            this.facade.getNutrition(food.getID(), measure, observer);
+        }
     }
 
     @Override
     public void sendMessage(Food food, String size, MessageObserver observer) {
-
+        if (food == null) {
+            observer.update(new IllegalArgumentException("Food cannot be null"));
+        } else if (size == null || size.equals("")) {
+            observer.update(new IllegalArgumentException("Size cannot be empty"));
+        } else {
+            this.facade.sendMessage(food.generateReport(size), observer);
+        }
     }
 }
