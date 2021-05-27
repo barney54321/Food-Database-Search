3d461107037049d3dceb11ad9d1dbbc66c0e820d
Andrew Esteban <andrewesteban1999@gmail.com>
2021-05-27 22:14:31 +1000

    created base cache methods

--- src/main/java/food/model/cache/Database.java
+++ src/main/java/food/model/cache/Database.java
@@ -1,5 +1,8 @@
 package food.model.cache;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Defines behaviour for the SQLite based cache.
  */
@@ -10,6 +13,21 @@ public interface Database {
      * @throws IllegalStateException If the connection cannot be formed.
      */
     void setup() throws IllegalStateException;
+
+    /**
+     * Updates the database with the provided string.
+     * @param update The update to run.
+     * @throws SQLException If there are any exceptions raised.
+     */
+    void executeUpdate(String update) throws SQLException;
+
+    /**
+     * Runs the provided query through the database.
+     * @param query The query to call.
+     * @return The resulting set.
+     * @throws SQLException If there is an exception raised while attempting to query.
+     */
+    ResultSet executeQuery(String query) throws SQLException;
 
     /**
      * Closes the Database connection.
