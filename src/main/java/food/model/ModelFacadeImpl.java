a031b627c26d815548fe0d6e343469f711584583
Andrew Esteban <andrewesteban1999@gmail.com>
2021-05-29 19:00:26 +1000

    GREEN: ModelFacade added concurrency methods

--- src/main/java/food/model/ModelFacadeImpl.java
+++ src/main/java/food/model/ModelFacadeImpl.java
@@ -9,6 +9,7 @@ import food.view.observers.FoodListObserver;
 import food.view.observers.MessageObserver;
 import food.view.observers.NutritionObserver;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -28,6 +29,16 @@ public class ModelFacadeImpl implements ModelFacade {
     private Twilio twilio;
 
     /**
+     * Whether the run() method should continue running.
+     */
+    private boolean run;
+
+    /**
+     * The list of tasks to be executed within run().
+     */
+    private List<Runnable> tasks;
+
+    /**
      * Creates a new ModelFacadeImpl object.
      * @param database The FoodDatabase the Facade will use.
      * @param twilio The Twilio the facade will use.
@@ -35,6 +46,8 @@ public class ModelFacadeImpl implements ModelFacade {
     public ModelFacadeImpl(FoodAPI database, Twilio twilio) {
         this.database = database;
         this.twilio = twilio;
+        this.run = true;
+        this.tasks = new ArrayList<>();
     }
 
     @Override
@@ -64,26 +77,39 @@ public class ModelFacadeImpl implements ModelFacade {
 
     @Override
     public void run() {
+        while (run) {
 
+            if (this.tasks.size() > 0) {
+                Runnable task = tasks.remove(0);
+                task.run();
+            }
+
+            // This is just here so that the loop doesn't cache run
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                ;
+            }
+        }
     }
 
     @Override
     public void stop() {
-
+        this.run = false;
     }
 
     @Override
     public void queueSearch(String term, boolean useCache, FoodListObserver observer) {
-
+        this.tasks.add(() -> this.search(term, useCache, observer));
     }
 
     @Override
     public void queueGetNutrition(String foodID, String measure, boolean useCache, NutritionObserver observer) {
-
+        this.tasks.add(() -> this.getNutrition(foodID, measure, useCache, observer));
     }
 
     @Override
     public void queueSendMessage(String message, MessageObserver observer) {
-
+        this.tasks.add(() -> this.sendMessage(message, observer));
     }
 }
